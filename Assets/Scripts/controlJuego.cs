dfe10b849dc20c07565748f1bb8186561125c372
yerayrl93 <yerayrl1993@gmail.com>
2025-11-24 12:27:22 +0100

    dia24/11

--- Assets/Scripts/controlJuego.cs
+++ Assets/Scripts/controlJuego.cs
@@ -1,12 +1,24 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class controlJuego : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private const float TIEMPO_INICIAL = 5f;
+    private float tiempoTranscurrido = TIEMPO_INICIAL;
+    [SerializeField] private TextMeshProUGUI textoCronometro;
+    private bool juegoTerminado = false;
+   
     void Start()
     {
-        
+        Time.timeScale = 1f;
+        if (textoCronometro != null)
+        {
+            int minutos = Mathf.FloorToInt(tiempoTranscurrido / 60F);
+            int segundos = Mathf.FloorToInt(tiempoTranscurrido % 60F);
+            textoCronometro.text = string.Format("{0:00}:{1:00}", minutos, segundos);
+
+        }
     }
 
     // Update is called once per frame
@@ -15,7 +27,33 @@ public class controlJuego : MonoBehaviour
         if (Input.GetKey(KeyCode.A))
         {
             SceneManager.LoadScene("Juego");
+
         }
+
+        if (Time.timeScale > 0)
+        {
+            tiempoTranscurrido -= Time.deltaTime;
+            if(tiempoTranscurrido <= 0f )
+            {
+                tiempoTranscurrido = 0f;
+                FinDelJuego(false);
+                return;
+            }
+
+            if (textoCronometro != null)
+            {
+                int minutos = Mathf.FloorToInt(tiempoTranscurrido / 60F);
+                int segundos = Mathf.FloorToInt(tiempoTranscurrido % 60F);
+                textoCronometro.text = string.Format("{0:00}:{1:00}", minutos, segundos);
+            }
+        }
+    }
+    private void FinDelJuego(bool victoria)
+    {
+        if (juegoTerminado) return;
+        juegoTerminado = true;
+        Time.timeScale = 0f; 
+        
     }
 }
 
